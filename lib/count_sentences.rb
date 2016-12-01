fd234918fde040b57226806800de6a89cb568519
kevwang424 <kev.wang424@gmail.com>
2016-11-30 20:25:38 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
-
+require 'byebug'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.gsub(/[!.?]/,"/").split("/").delete_if{|element|element==""}.count
   end
+
 end
